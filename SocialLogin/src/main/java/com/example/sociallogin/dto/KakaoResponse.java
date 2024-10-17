7583a7e67d9fcc7561b38a7ac6d143b4a6a0e998
hajew <hajewoong@gmail.com>
2024-10-17 12:02:37 +0900

    kakao 수정

--- SocialLogin/src/main/java/com/example/sociallogin/dto/KakaoResponse.java
+++ SocialLogin/src/main/java/com/example/sociallogin/dto/KakaoResponse.java
@@ -21,15 +21,17 @@ public class KakaoResponse implements OAuth2Response {
 
     /**
      * 현재 카카오 앱 인증을 받지 않아서 이메일을 받아올 수 없음
+     *
      * @return 카카오 이메일
      */
     @Override
     public String getEmail() {
-        return "";
+        return attribute.get("account_email").toString();
     }
 
     /**
      * 현재 카카오 앱 인증을 받지 않아서 실명을 받아올 수 없음
+     *
      * @return 카카오 유저 실명
      */
     @Override
@@ -41,7 +43,7 @@ public class KakaoResponse implements OAuth2Response {
     /**
      * @return 카카오 프로필 닉네임
      */
-    public String getNickName(){
+    public String getNickName() {
         return attribute.get("profile_nickname").toString();
     }
 }
