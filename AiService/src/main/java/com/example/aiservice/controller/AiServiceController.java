b04816a42cce15873c61d0191a54c48fd4e01669
haejun <codingjun@naver.com>
2024-10-18 01:46:10 +0900

    spring cloud setting

--- AiService/src/main/java/com/example/aiservice/controller/AiServiceController.java
+++ AiService/src/main/java/com/example/aiservice/controller/AiServiceController.java
@@ -4,8 +4,10 @@ import com.example.aiservice.service.STTService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 @RestController
@@ -17,7 +19,16 @@ public class AiServiceController {
     }
 
     @PostMapping("/stt")
-    public String index(@RequestParam("file") File file) throws URISyntaxException {
+    public String index(@RequestParam("file") MultipartFile multipartFile) throws URISyntaxException, IOException {
+        // MultipartFile을 File로 변환
+        File file = convertToFile(multipartFile);
         return STTService.stt(file);
+    }
+
+    private File convertToFile(MultipartFile multipartFile) throws IOException {
+        // 임시 파일 생성
+        File tempFile = File.createTempFile("uploaded", multipartFile.getOriginalFilename());
+        multipartFile.transferTo(tempFile);
+        return tempFile;
     }
 }
