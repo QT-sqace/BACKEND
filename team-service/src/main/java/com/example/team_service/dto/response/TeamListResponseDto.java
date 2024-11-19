e4a6f5b7d0f5b06314e417462630067ef8552249
haejun <codingjun@naver.com>
2024-11-19 23:24:22 +0900

    팀 리스트 반환 수정

--- team-service/src/main/java/com/example/team_service/dto/response/TeamListResponseDto.java
+++ team-service/src/main/java/com/example/team_service/dto/response/TeamListResponseDto.java
@@ -11,6 +11,7 @@ public class TeamListResponseDto {
     private Long teamId;                //팀 ID
     private String teamName;            //팀 이름
     private String teamImage;           //팀 이미지 경로
+    private String role;                //팀 내 사용자 역할
     private int memberCount;            //팀 전체 인원수
     private List<String> memberImages;  //팀 회원들 이미지 경로들
 }
