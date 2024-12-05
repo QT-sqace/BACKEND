41cb38dd8f3a62501a57792dbab578bea143624a
haejun <162435572+HaejunJang@users.noreply.github.com>
2024-12-05 12:52:21 +0900

    Update TeamMemberService.java

--- team-service/src/main/java/com/example/team_service/service/TeamMemberService.java
+++ team-service/src/main/java/com/example/team_service/service/TeamMemberService.java
@@ -1,8 +1,5 @@
 package com.example.team_service.service;
 
-
-import com.example.team_service.dto.external.TeamMemberInfoDto;
-
 import com.example.team_service.entity.TeamMember;
 import com.example.team_service.repository.TeamMemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +7,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import java.util.stream.Collectors;
-
-
 @Service
 @RequiredArgsConstructor
 public class TeamMemberService {
 
     private final TeamMemberRepository teamMemberRepository;
-
 
     // ID로 특정 멤버 조회
     public TeamMember getTeamMemberById(Long teamMemberId) {
@@ -34,14 +27,5 @@ public class TeamMemberService {
     // 특정 유저가 속한 팀 멤버십 정보 조회
     public List<TeamMember> getAllTeamsByUserId(Long userId) {
         return teamMemberRepository.findAllByUserId(userId);
-
-    public List<TeamMemberInfoDto> getTeamMembers(Long teamId) {
-        List<TeamMember> teamMembers = teamMemberRepository.findByTeam_TeamId(teamId);
-        return teamMembers.stream()
-                .map(member -> new TeamMemberInfoDto(
-                        member.getUserId(),
-                        member.getRole().name()))
-                .collect(Collectors.toList());
-
     }
 }
