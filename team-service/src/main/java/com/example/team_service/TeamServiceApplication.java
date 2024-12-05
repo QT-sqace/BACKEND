1e2e356a93ffdf7fb933f80eb3c8b84f5cd897cd
haejun <162435572+HaejunJang@users.noreply.github.com>
2024-12-05 11:17:34 +0900

    Update TeamServiceApplication.java

--- team-service/src/main/java/com/example/team_service/TeamServiceApplication.java
+++ team-service/src/main/java/com/example/team_service/TeamServiceApplication.java
@@ -3,10 +3,13 @@ package com.example.team_service;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class TeamServiceApplication {
 
@@ -14,9 +17,10 @@ public class TeamServiceApplication {
 		SpringApplication.run(TeamServiceApplication.class, args);
 	}
 
-	// Feign 로깅 레벨 설정
 	@Bean
 	public Logger.Level feignLoggerLevel() {
 		return Logger.Level.FULL;
 	}
 }
+
+
