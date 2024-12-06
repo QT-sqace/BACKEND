e251ede96f1b48ea774048adf3a2f522f6c39f0a
Limchaeyoon425 <banajulie@naver.com>
2024-12-06 19:53:35 +0900

    TeamMember 엔티티에서 joined_at의 밀리초 제거 및 관련 로직 수정

--- team-service/src/main/java/com/example/team_service/service/TeamMemberService.java
+++ team-service/src/main/java/com/example/team_service/service/TeamMemberService.java
@@ -1,24 +1,19 @@
 package com.example.team_service.service;
 
-
 import com.example.team_service.dto.external.TeamMemberInfoDto;
-
 import com.example.team_service.entity.TeamMember;
 import com.example.team_service.repository.TeamMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 import java.util.stream.Collectors;
-
 
 @Service
 @RequiredArgsConstructor
 public class TeamMemberService {
 
     private final TeamMemberRepository teamMemberRepository;
-
 
     // ID로 특정 멤버 조회
     public TeamMember getTeamMemberById(Long teamMemberId) {
@@ -34,14 +29,15 @@ public class TeamMemberService {
     // 특정 유저가 속한 팀 멤버십 정보 조회
     public List<TeamMember> getAllTeamsByUserId(Long userId) {
         return teamMemberRepository.findAllByUserId(userId);
+    }
 
+    // 특정 팀의 멤버 정보를 DTO로 반환
     public List<TeamMemberInfoDto> getTeamMembers(Long teamId) {
-        List<TeamMember> teamMembers = teamMemberRepository.findByTeam_TeamId(teamId);
+        List<TeamMember> teamMembers = teamMemberRepository.findAllByTeamTeamId(teamId);
         return teamMembers.stream()
                 .map(member -> new TeamMemberInfoDto(
                         member.getUserId(),
                         member.getRole().name()))
                 .collect(Collectors.toList());
-
     }
 }
