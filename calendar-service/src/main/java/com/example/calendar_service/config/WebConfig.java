8e360150109975a3b0a9ffff6e749e8000772a56
hajew <hajewoong@gmail.com>
2024-12-09 18:01:51 +0900

    cors경로 수정

--- calendar-service/src/main/java/com/example/calendar_service/config/WebConfig.java
+++ calendar-service/src/main/java/com/example/calendar_service/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://172.17.211.23")
+                .allowedOrigins("http://172.16.211.23")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
