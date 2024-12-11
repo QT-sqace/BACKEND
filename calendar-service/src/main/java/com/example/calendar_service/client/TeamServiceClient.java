62f1705fc0c8b2219aada9c4e87595ee29b200bb
hajew <hajewoong@gmail.com>
2024-12-11 14:58:46 +0900

    페인 클라이언트 url수정

--- calendar-service/src/main/java/com/example/calendar_service/client/TeamServiceClient.java
+++ calendar-service/src/main/java/com/example/calendar_service/client/TeamServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "team-service-client", url = "http://localhost:8000/teamservice")
+@FeignClient(name = "team-service-client", url = "team-service.spring-boot-app.svc.cluster.local:8084")
 public interface TeamServiceClient {
 
     //팀 일정 등록시점에 팀 서비스로 팀에 속한 userId가져오기
