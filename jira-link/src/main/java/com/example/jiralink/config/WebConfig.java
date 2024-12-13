1ed041ff9d4aaca14985546c7d3792c3ebee3ab5
chan9e <pmkrhgh@gmail.com>
2024-12-13 10:50:43 +0900

    attlasian-connect.json 추가

---
+++ jira-link/src/main/java/com/example/jiralink/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.jiralink.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UserStatusInterceptor userStatusInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
