e3dc9f69af21447a75608a6442d561dc3faa44a4
chan9e <pmkrhgh@gmail.com>
2024-12-13 11:16:24 +0900

    WebConfig 수정

--- jira-link/src/main/java/com/example/jiralink/config/WebConfig.java
+++ jira-link/src/main/java/com/example/jiralink/config/WebConfig.java
@@ -9,8 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final UserStatusInterceptor userStatusInterceptor;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
