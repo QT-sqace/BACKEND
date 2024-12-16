70a8d5be5d665702ebb9c928bdbd14bbf4b33c5b
hajew <hajewoong@gmail.com>
2024-12-16 16:37:48 +0900

    chatservice

--- chat_service/src/main/java/com/example/chat_service/ChatServiceApplication.java
+++ chat_service/src/main/java/com/example/chat_service/ChatServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.chat_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
